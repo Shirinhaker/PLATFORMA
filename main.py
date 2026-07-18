50dad1a18cfec380fbbd35c58276ace585da72d6
Shirinhaker <mashdam01@gmail.com>
2026-07-18 14:07:12 +0500

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ from catalog_data import CATALOG, LISTING_CATS
 from access_config import PRIVILEGED_TG_IDS, is_privileged_tg_id
 
 # ---------- Sozlamalar ----------
-APP_BUILD = "v1591"
+APP_BUILD = "v1592"
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 
