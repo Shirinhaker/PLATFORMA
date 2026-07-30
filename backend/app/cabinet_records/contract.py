cf9bbd210112c1b834aa47bd7231b3a835f7972b
Shirinhaker <mashdam01@gmail.com>
2026-07-31 01:25:28 +0500

    test: add red normalization contract placeholder

---
+++ backend/app/cabinet_records/contract.py
@@ -0,0 +1,6 @@
+"""V7 cabinet normalization contract constants.
+
+RED bosqichda implementatsiya keyingi commitlarda qo‘shiladi.
+"""
+
+NORMALIZATION_SCHEMA_VERSION = "0006_v7_normalized_cabinet_records"
